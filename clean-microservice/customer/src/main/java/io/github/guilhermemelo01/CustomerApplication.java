10dcc6de17e4846432da54c269c2aa89c5f1f207
GuilhermeMelo01 <meloguilherme1994@gmail.com>
2023-11-12 14:46:47 -0300

    Eureka discovery and Load balanced

--- clean-microservice/customer/src/main/java/io/github/guilhermemelo01/CustomerApplication.java
+++ clean-microservice/customer/src/main/java/io/github/guilhermemelo01/CustomerApplication.java
@@ -2,7 +2,9 @@ package io.github.guilhermemelo01;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class CustomerApplication {
     public static void main(String[] args) {
